cb2537411a4e5e384d6d2ebe03ab283c7ae3322d
Albert Oliva <albert.oliva@gmail.com>
2026-02-03 09:10:37 +0200

    Fix: Corregir error de TypeScript en FoundationInit con importación dinámica de jQuery

--- src/app/FoundationInit.tsx
+++ src/app/FoundationInit.tsx
@@ -5,13 +5,14 @@ import { useEffect } from 'react';
 const FoundationInit = () => {
   useEffect(() => {
     // Use dynamic import to ensure jQuery and Foundation JS are loaded only on the client side.
-    import('jquery').then($ => {
+    import('jquery').then(($) => {
+      const jQuery = $.default as any;
       // @ts-ignore
-      window.jQuery = $.default;
+      window.jQuery = jQuery;
       // require() is needed for modules that are not ES6 compatible.
       require('foundation-sites/dist/js/foundation.min.js');
       // Initialize Foundation
-      $(document).foundation();
+      jQuery(document).foundation();
     });
   }, []);
 
