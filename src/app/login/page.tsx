1d2551ba70653db1aa5bd49e6236dacf01ed0ab2
Albert Oliva <albert.oliva@gmail.com>
2026-02-08 11:55:59 +0200

    debug: Remove useOneTap to isolate 'origin not allowed' error

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -100,7 +100,6 @@ export default function LoginPage() {
           <GoogleLogin
             onSuccess={handleGoogleSuccess}
             onError={handleGoogleError}
-            useOneTap
           />
         </div>
 
